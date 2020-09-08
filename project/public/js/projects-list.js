75230cc81ef89e0ea06681cd857ed7f20724dbdd
sholt20 <samuelholt123@gmail.com>
2020-09-08 19:16:33 -0400

    Set up event listeners on the list items to have descriptions pop out.
    Descriptions do not come out yet, but the event listeners work.

--- project/public/js/projects-list.js
+++ project/public/js/projects-list.js
@@ -1,12 +1,28 @@
 window.addEventListener("DOMContentLoaded", async event => {
-     let list = document.querySelector('.projects');
+     console.log('test')
+     let list = document.querySelector('.list-group');
      const res = await fetch("/projects-data");
      const projects = await res.json();
-     projects.forEach(project => {
+     projects.forEach((project, i) => {
+          i++
+          let conDiv = document.createElement('div');
           let li = document.createElement('li');
-          li.classList.add('project');
-          li.innerHTML = project.projectName;
+          li.classList.add('list-group-item');
+          li.classList.add('project-items')
+          li.setAttribute('id', `Item-${i}`);
+          li.innerHTML = `${i}. ${project.projectName}`;
+          conDiv.appendChild(li);
 
-          list.appendChild(li);
+          list.appendChild(conDiv);
      });
+     document.getElementById('list-area')
+          .addEventListener('click', e => {
+               let target = e.target.id;
+               let element = document.getElementById(target);
+               if (element.classList.contains('selected')) {
+                    element.classList.remove('selected')
+                    return;
+               }
+               element.classList.add('selected')
+          });
 });
