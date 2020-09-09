b4cb7569e8ca903b59198f8a2679ae478d23c76e
sholt20 <samuelholt123@gmail.com>
2020-09-09 13:46:40 -0400

    Details section now pulls various pieces of information from the database and is properly populated with that data.

--- project/public/js/projects.js
+++ project/public/js/projects.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", async event => {
+     // Sam - Populate the projects list with the data from the database
      let list = document.querySelector('.list-group');
      const res = await fetch("/projects-data");
      const projects = await res.json();
@@ -14,7 +15,7 @@ window.addEventListener("DOMContentLoaded", async event => {
 
           list.appendChild(conDiv);
      });
-
+     // Sam - Event handler to open up stats/details on click of a list element and populate details data
      document.getElementById('list-area')
           .addEventListener('click', e => {
                let target = e.target.id;
@@ -40,9 +41,45 @@ window.addEventListener("DOMContentLoaded", async event => {
                let projId = Number(target);
                projects.forEach(project => {
                     if (projId === project.id) {
-                         let desc = document.getElementById('description');
-                         desc.innerHTML = project.description;
+                         populateDetails(project)
                     }
                });
           });
 });
+
+// Sam- function to remove the time stamp from the databases date entries
+function splitDate(date) {
+     let arr = date.split('T');
+     let newDate = arr[0];
+     return newDate;
+}
+
+// Sam - function to set data in the details section and fetch team names based on team id
+async function populateDetails(project) {
+     let start = document.getElementById('start-date');
+     let due = document.getElementById('due-date');
+     let list = document.getElementById('list-entry');
+     let team = document.getElementById('team-name');
+     let details = document.getElementById('details-area');
+     let title = document.getElementById('project-title');
+
+     let res = await fetch('/team-names');
+     let teamNames = await res.json();
+     let teamName;
+     if (project.teamId === undefined) {
+          teamName === 'Project not assigned'
+     } else {
+          teamNames.forEach(team => {
+               if (team.id === project.teamId) {
+                    teamName = team.name;
+               }
+          });
+     }
+
+     title.innerHTML = project.projectName;
+     start.innerHTML = splitDate(project.createdAt);
+     due.innerHTML = splitDate(project.deadline);
+     list.innerHTML = "Add when lists/tags are a thing"
+     team.innerHTML = teamName;
+     details.innerHTML = project.description;
+}
