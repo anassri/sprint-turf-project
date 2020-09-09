a4b3b47cc87c4afacd3f45e15dedf0b7a096c404
Matt Zamora <mzamora2899@gmail.com>
2020-09-09 11:20:55 -0600

    add search functionality

--- project/public/js/searchBar.js
+++ project/public/js/searchBar.js
@@ -1,11 +1,21 @@
 const searchBar = document.getElementById('searchBar');
-const list = document.getElementById('searchList');
+const list = document.querySelector('.list-group');
 let projectNames = [];
 
 searchBar.addEventListener('keyup', (event) => {
-  const searchString = event.target.value
-  const filteredProjects = projectNames.filter( project => {
-    return project.name.includes(searchString)
+  const searchString = event.target.value.toLowerCase();
+  const projects = list.getElementsByTagName('li');
+  Array.from(projects).forEach((project) => {
+    const name = project.projectName;
+    if (name.toLowerCase().indexOf(searchString) !== -1) {
+      project.style.display = 'block';
+    } else {
+      book.style.display = 'none'
+    }
   })
 
 })
+
+const projectName = async () => {
+
+}
