02e9786a92282bdb9a5f42897912a8cc62644579
Matt Zamora <mzamora2899@gmail.com>
2020-09-12 21:33:53 -0600

    finish fixing and styling

--- project/public/js/searchBar.js
+++ project/public/js/searchBar.js
@@ -1,12 +1,18 @@
 const searchBar = document.getElementById('searchBar');
 const list = document.querySelector('.list-group');
 
-searchBar.addEventListener('keyup', (event) => {
+searchBar.addEventListener('keydown', (event) => {
   // grab the string typed in the search bar
   const searchString = event.target.value.toLowerCase();
   // grab all of the li elements generated in the ul, each the li elements will be every seperate projoect
   const projects = list.getElementsByTagName('div');
 
+  searchBar.classList.remove('search-bar');
+
+  searchBar.addEventListener('blur', (event) => {
+    searchBar.classList.add('search-bar');
+    searchBar.value = "";
+  })
   // create an array from these projects to iterate
   Array.from(projects).forEach((project) => {
     // console.log("project------",project)
