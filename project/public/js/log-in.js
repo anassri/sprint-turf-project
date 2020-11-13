10122a8f661712612166ef291bdd897e2550f0a0
Yongho Kim <winthisgame12@gmail.com>
2020-11-12 17:44:00 -0700

    add demo user

--- project/public/js/log-in.js
+++ project/public/js/log-in.js
@@ -1,9 +1,43 @@
 import { handleErrors } from "./utils.js"
 window.addEventListener("DOMContentLoaded", async (e) => {
-  //const logIn = document.querySelector(".log-in-form");
+ 
   const logIn = document.getElementById("login");
+  const demoUser = document.getElementById("demo-user");
+
+  demoUser.addEventListener("click", async (e) => {
+    e.preventDefault();
+    console.log("demo user")
+    const email = "email@gmail.com";
+    const password = "1234";
+    const body = { email, password };
+
+    try {
+      const res = await fetch("/users/token", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!res.ok) {
+        throw res;
+      }
+      const {
+        token,
+        user: { id },
+      } = await res.json();
+
+      localStorage.setItem("SPRINT_TURF_ACCESS_TOKEN", token);
+      localStorage.setItem("SPRINT_TURF_CURRENT_USER_ID", id);
+
+      window.location.href = "/";
+    } catch (err) {
+      handleErrors(err);
+    }
+
+  })
   
-  // const logInButton = document.querySelector(".btn-dark");
 
   logIn.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -37,5 +71,6 @@ window.addEventListener("DOMContentLoaded", async (e) => {
       handleErrors(err);
     }
   });
+  
   
 });
